636ff42cd775832aa8320f649f6dc786c079f92c
Greg Hurrell <greg@causes.com>
2012-02-27 21:36:30 -0800

    Add vanilla Rails 3.2.1 app as a demo testbed
    Result of running:
    
      rails new lightning
    
    after setting up the appropriate .rvmrc file.
    
    Signed-off-by: Greg Hurrell <greg@causes.com>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lightning::Application.config.secret_token = '2a3a23433bcedcc529fe9cab48815aa300fb6c798808bf1b81c29c9e401d8df0e631665864f60ed58294f7eff4852b1b590cc0f4523281830fa2b5fe1848b7d3'
