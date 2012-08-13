8ef56e381a3d8665b0b31344740862b2313a8bb7
Greg Hurrell <greg@causes.com>
2012-08-13 13:11:39 -0700

    Add more precompiled assets
    The last commit ended up only including a subset of the precompiled
    assets. To truly get them all we need to compile using:
    
      RAILS_ENV=production bundle exec rake assets:precompile
    
    Note that some tweaks were required to the application config
    (explicitly listing the stylesheet files we wanted to precompile, and
    renaming them to follow the expected conventions).

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module Lightning
   class Application < Rails::Application
     config.assets.enabled = true
     config.assets.initialize_on_precompile = false
+    config.assets.precompile += %w(home.css ie.css print.css screen.css)
     config.assets.version = '1.0'
     config.encoding = "utf-8"
     config.filter_parameters += [:password]
